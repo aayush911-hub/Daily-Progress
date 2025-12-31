f66e3b1552cfc90e8f1eb43389f8dac19f75bb82
Aayush Ojha <aayushojha073@gmail.com>
2025-12-31 15:19:07 +0530

    Day-2

--- Day-2/Python Questions.py
+++ Day-2/Python Questions.py
@@ -17,11 +17,45 @@
 
 # -----------------------------------------------------------
 # Important
-a = [1, 2]
-b = a
-b = b + [3]
-print(a)
+# a = [1, 2]
+# b = a
+# b = b + [3]
+# print(a)
 
 # here 'b' creates a new object
 # --------------------------------------------------------------
+
+# a = list((1, 2, 3))
+# print(a) # [1, 2, 3] -> Set converted into list
+
+# a = (1, 2, 3)
+# b = (4, 5, 6)
+# c = list([a,b])
+# dif = c[1] # this shows that c is list from outer block but the inner block is tuple only
+# print(dif)
+# ---------------------------------------------
+
+# a = [[1, 2], [3, 4]] # let [1, 2] -> e, [3, 4] -> f
+# b = a.copy()
+# b[0] = [9, 9] # here we didn't access the inner objects a[0][0] or a[0][1] or a[1][0] or a[1][1], we just changed the outer container's element [e,f].
+# print(a)
+
+# ---------------------------------------------------
+
+def f(a, cache = []):
+    cache.append(a)
+    return cache
+
+f(4) # [4]
+v = f(6) # [4, 6]
+print(v) # [4, 6]
+
+def fv(b, cache1 = None):
+    if cache1 is None:
+        cache1 = []
+    cache1.append(b)
+    return cache1
+fv(5) # [5]
+value = fv(8) # [8]
+print(value)
 
